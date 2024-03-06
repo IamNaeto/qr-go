bc7195523eef076809a57278ee3ea0965ec5bd18
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-03-06 03:12:51 -0800

    minor fixes on the landing page

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -5,7 +5,8 @@ import GetStartedBtn from "./GetStartedBtn";
 
 const LandingHero = () => {
     return (
-        <main className="w-full px-[5%] relative top-[80px] py-7">
+        <main className="w-full min-h-screen relative top-[80px] grid place-items-center">
+        <main className="w-full px-[5%] py-7">
             <section className="w-full grid grid-cols-1 md:grid-cols-2 items-center justify-center place-items-center">
                 <section className="grid gap-4">
                     <h1 className="w-full lg:w-[80%] text-[28px] md:text-[36px] lg:text-[40px] xl:text-[42px] text-dark font-semibold text-center md:text-left"><span className="text-blue">Create a QR code</span> to
@@ -37,6 +38,7 @@ const LandingHero = () => {
 
                 <hr className="bg-darkblue h-1 w-full"/>
             </section>
+        </main>
         </main>
     );
 }
