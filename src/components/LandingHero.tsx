5a0af6a108c633bf0d974550cbdb669d9ccc1989
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-03-11 03:08:31 -0700

    Fixes

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -6,39 +6,39 @@ import GetStartedBtn from "./GetStartedBtn";
 const LandingHero = () => {
     return (
         <main className="w-full min-h-screen relative top-[80px] grid place-items-center">
-        <main className="w-full px-[5%] py-7">
-            <section className="w-full grid grid-cols-1 md:grid-cols-2 items-center justify-center place-items-center">
-                <section className="grid gap-4">
-                    <h1 className="w-full lg:w-[80%] text-[28px] md:text-[36px] lg:text-[40px] xl:text-[42px] text-dark font-semibold text-center md:text-left"><span className="text-blue">Create a QR code</span> to
-                        both Safeguard your data
-                        and <span className="text-blue">advertise</span> your <span>business</span> or
-                        <span className="text-blue"> idea.</span></h1>
+            <main className="w-full px-[5%] py-7">
+                <section className="w-full grid grid-cols-1 md:grid-cols-2 items-center justify-center place-items-center">
+                    <section className="grid gap-4">
+                        <h1 className="w-full lg:w-[90%] text-[28px] md:text-[36px] lg:text-[40px] xl:text-[42px] text-dark font-bold text-center md:text-left"><span className="text-blue">Create a QR code</span> to
+                            both Safeguard your data
+                            and <span className="text-blue">advertise</span> your <span>business</span> or
+                            <span className="text-blue"> idea.</span></h1>
 
-                    <p className="w-full lg:w-[80%] text-[14px] md:text-[16px] lg:text-[18px] text-dark font-medium text-center md:text-left">Our platform allows you to effortlessly generate QR codes,
-                        keep track of them, and analyze their performance
-                        through detailed statistics.</p>
+                        <p className="w-full lg:w-[80%] text-[14px] md:text-[16px] lg:text-[18px] text-dark font-medium text-center md:text-left">Our platform allows you to effortlessly generate QR codes,
+                            keep track of them, and analyze their performance
+                            through detailed statistics.</p>
 
-                    <div className="flex items-center justify-center md:justify-start gap-4">
-                        <GetStartedBtn />
-                        <Link href="/home" className="text-[14px] md:text-[16px] px-3 md:px-5 py-2 bg-white text-blue border-2 border-blue font-semibold rounded-md hover:shadow-lg transition-all delay-150">Learn More</Link>
+                        <div className="flex items-center justify-center md:justify-start gap-4">
+                            <GetStartedBtn />
+                            <Link href="/home" className="text-[14px] md:text-[16px] px-3 md:px-5 py-2 bg-white text-blue border-2 border-blue font-semibold rounded-md hover:shadow-lg transition-all delay-150">Learn More</Link>
+                        </div>
+                    </section>
+
+                    <section>
+                        <Image src="/img/landing-img.png" width={580} height={580} alt="hero-img" loading="lazy" />
+                    </section>
+                </section>
+
+                <section className="flex items-center gap-6 mt-6">
+                    <div className="flex items-center gap-4">
+                        <FaXTwitter className="text-dark text-3xl p-1.5 rounded-full bg-skyblue hover:bg-darkblue hover:text-white transition-all delay-200 cursor-pointer" />
+                        <FaFacebookF className="text-dark text-3xl p-1.5 rounded-full bg-skyblue hover:bg-darkblue hover:text-white transition-all delay-200 cursor-pointer" />
+                        <FaYoutube className="text-dark text-3xl p-1.5 rounded-full bg-skyblue hover:bg-darkblue hover:text-white transition-all delay-200 cursor-pointer" />
                     </div>
+
+                    <hr className="bg-darkblue h-1 w-full" />
                 </section>
-
-                <section>
-                    <Image src="/img/landing-img.png" width={580} height={580} alt="hero-img"  loading="lazy"/>
-                </section>
-            </section>
-
-            <section className="flex items-center gap-6 mt-6">
-                <div className="flex items-center gap-4">
-                    <FaXTwitter className="text-dark text-3xl p-1.5 rounded-full bg-skyblue hover:bg-darkblue hover:text-white transition-all delay-200 cursor-pointer"/>
-                    <FaFacebookF className="text-dark text-3xl p-1.5 rounded-full bg-skyblue hover:bg-darkblue hover:text-white transition-all delay-200 cursor-pointer"/>
-                    <FaYoutube className="text-dark text-3xl p-1.5 rounded-full bg-skyblue hover:bg-darkblue hover:text-white transition-all delay-200 cursor-pointer"/>
-                </div>
-
-                <hr className="bg-darkblue h-1 w-full"/>
-            </section>
-        </main>
+            </main>
         </main>
     );
 }
--- src/components/SideView.tsx
+++ src/components/SideView.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 const SideView = () => {
     return ( 
         <main className="h-full">
             <section className="bg-liteskyblue flex items-center justify-center py-5 md:py-20 h-full min-h-auto md:min-h-screen">
-                <Image src="/img/contactus.png" width={400} height={400} alt="aboutus-img" loading="lazy"/>
+                <Link href={"/"}>
+                    <Image src="/img/contactus.png" width={400} height={400} alt="aboutus-img" loading="lazy"/>
+                </Link>
             </section>
         </main>
      );
