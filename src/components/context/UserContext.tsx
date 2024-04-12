bd57ce5c34f2a7cba18421d043d8aab0abb9a49d
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-04-12 21:41:26 +0100

    implement rapid fetching of data on change or update and added loading state

--- src/components/context/UserContext.tsx
+++ src/components/context/UserContext.tsx
@@ -1,7 +1,8 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { Auth, User, getAuth, onAuthStateChanged } from 'firebase/auth';
-import { doc, DocumentSnapshot, getDoc } from 'firebase/firestore';
+import { doc, DocumentData, DocumentSnapshot, getDoc, onSnapshot } from 'firebase/firestore';
 import { db } from '@/app/firebase/config';
+import { TbLoader3 } from 'react-icons/tb';
 
 // Define your user data type from Firestore
 interface UserDataType {
@@ -29,13 +30,16 @@ interface UserDataType {
 }
 
 // Update the UserContextState interface to match your user data type
-export interface UserContextState {
-  user: UserDataType | null | any;
+interface UserContextState {
+  user: UserDataType | null;
+  isLoading: boolean;
 }
+
 
 
 export const UserContext = createContext<UserContextState>({
   user: null,
+  isLoading: true,
 });
 
 interface UserProviderProps {
@@ -44,38 +48,59 @@ interface UserProviderProps {
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [user, setUser] = useState<UserDataType | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const auth: Auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+    const unsubscribeAuth = onAuthStateChanged(auth, async (currentUser) => {
       if (currentUser) {
+        setIsLoading(true); // Set loading to true when starting data fetch
         await fetchUser(currentUser.uid);
+        setIsLoading(false); // Set loading to false after data fetch
+        // Add listener for changes in Firestore user document
+        const userDocRef = doc(db, 'users', currentUser.uid);
+        const unsubscribeSnapshot = onSnapshot(userDocRef, (doc: DocumentSnapshot<DocumentData>) => {
+          if (doc.exists()) {
+            const updatedUserData = doc.data() as UserDataType;
+            setUser(updatedUserData);
+          }
+        });
+        return () => unsubscribeSnapshot();
       } else {
         setUser(null);
       }
     });
 
-    return () => unsubscribe();
+    return () => unsubscribeAuth();
   }, []);
 
   const fetchUser = async (userId: string) => {
-    const docRef = doc(db, "users", userId);
+    const userDocRef = doc(db, 'users', userId);
     try {
-      const docSnap: DocumentSnapshot = await getDoc(docRef);
+      const docSnap: DocumentSnapshot<DocumentData> = await getDoc(userDocRef);
       if (docSnap.exists()) {
         const userData = docSnap.data() as UserDataType;
         setUser(userData);
       } else {
-        console.error("User document not found");
+        console.error('User document not found');
       }
     } catch (error) {
-      console.error("Error fetching user:", error);
+      console.error('Error fetching user:', error);
     }
   };
 
   return (
-    <UserContext.Provider value={{ user }}>
-      {children}
+    <UserContext.Provider value={{ user, isLoading }}>
+      {isLoading ? ( // Render loading spinner if isLoading is true
+        <div className="w-full h-screen flex items-center justify-center bg-white text-center">
+          <TbLoader3
+            className="animate-spin text-orange text-7xl font-semibold text-center"
+          />
+        </div>
+      ) : (
+        // Render children if isLoading is false (data has been fetched)
+        children
+      )}
     </UserContext.Provider>
   );
 };
